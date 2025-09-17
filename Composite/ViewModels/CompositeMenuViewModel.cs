6bd18daa633f9a0f6cf554bc14db2a147b0ee977
Eddo <codingeprst@gmail.com>
2025-09-17 12:54:21 +0300

    Начал сырую перерисовку notes

--- Composite/ViewModels/CompositeMenuViewModel.cs
+++ Composite/ViewModels/CompositeMenuViewModel.cs
@@ -1,12 +1,26 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
 using Composite.Services;
 using Composite.Services.TabService;
 using Composite.ViewModels.Notes;
+using Composite.ViewModels.Notes.HardNote;
 
 namespace Composite.ViewModels
 {
     public partial class CompositeMenuViewModel(IViewService viewService, ITabService tabService)
     {
+
+        public ObservableCollection<NoteBaseVM> Collections { get; set; } = new()
+        {
+            new NoteVM() { Title = "Простая заметка 1" },
+            new NoteVM() { Title = "Простая заметка 2" },
+            new NoteVM() { Title = "Простая заметка 3" },
+            new NoteVM() { Title = "Простая заметка 4" },
+            new NoteVM() { Title = "Простая заметка 5" },
+
+            new HardNoteVM() { Title = "Функциональная заметка 1"},
+            new HardNoteVM() { Title = "Функциональная заметка 2"}
+        };
 
         [RelayCommand] void OpenNotes() => tabService.CreateTab<NotesViewModel>("Заметки");
 
